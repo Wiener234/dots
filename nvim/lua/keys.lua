9df6a7f18a5dcd84d3631854141c6b291b5cf02a
Wiener234 <wiemernils@gmail.com>
2023-09-05 19:02:56 +0200

    nvim_conf_doc add keys neorg gruvbox_conf

--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -1,6 +1,3 @@
--- [[ keys.lua ]]
-
-local vim = vim
 local map = vim.api.nvim_set_keymap
 
 map('i', 'jk', '<Esc>', {})				-- exit insert mode
@@ -13,13 +10,10 @@ map('n', 'N', ':Files ', {})			-- open fzf Files [user input]
 map('n', 'z', ':Buffers<CR>', {})
 map('n', 'Z', ':GFiles?<CR>', {})
 
+vim.keymap.set("n", "<localleader>p", require("nabla").popup, {})
+vim.keymap.set("n", "<localleader>s", require("nabla").toggle_virt, {})
 
--- map("n", "<localleader>p", require("nabla").popup, {})
--- map("n", "<localleader>s", require("nabla").toggle_virt, {})
-
-
-vim.api.nvim_create_autocmd('FileType', {
-	pattern = '*',
+vim.api.nvim_create_autocmd({'BuffEnter', 'TermEnter'} , {
 	callback = function ()
 		if vim.bo.filetype == 'fzf' then
 			vim.keymap.set('n', 'n', function ()
@@ -31,11 +25,16 @@ vim.api.nvim_create_autocmd('FileType', {
 				vim.api.nvim_command(':q')
 				vim.api.nvim_command(":lua vim.keymap.set('n', 'z', ':Buffers<CR>', {})")
 			end, {})
-
+			
 			vim.keymap.set('n', 'Z', function ()
 				vim.api.nvim_command(':q')
 				vim.api.nvim_command(":lua vim.keymap.set('n', 'Z', ':GFiles?<CR>', {})")
 			end, {})
+       else
+			vim.keymap.set('n', 'n', ':Files<CR>', {})			-- open fzf Files ./
+			vim.keymap.set('n', 'N', ':Files ', {})			-- open fzf Files [user input]
+			vim.keymap.set('n', 'z', ':Buffers<CR>', {})
+			vim.keymap.set('n', 'Z', ':GFiles?<CR>', {})
 		end
 	end,
 })
@@ -43,31 +42,30 @@ vim.api.nvim_create_autocmd('FileType', {
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+  	-- Enable completion triggered by <c-x><c-o>
+  	vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
-    -- Buffer local mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
-    end, opts)
-  end,
+  	-- Buffer local mappings.
+  	-- See `:help vim.lsp.*` for documentation on any of the below functions
+  	local opts = { buffer = ev.buf }
+  	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  	vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+  	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+  	vim.keymap.set('n', '<space>wl', function()
+  	  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  	end, opts)
+  	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+  	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+  	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+  	vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+  	vim.keymap.set('n', '<space>f', function()
+  	  vim.lsp.buf.format { async = true }
+  	end, opts)
+	end,
 })
-
---
+++ nvim/lua/keys_old.lua
@@ -0,0 +1,73 @@
+-- [[ keys.lua ]]
+
+local vim = vim
+local map = vim.api.nvim_set_keymap
+
+map('i', 'jk', '<Esc>', {})				-- exit insert mode
+map('v', 'jk', '<Esc>', {})				-- exit visual mode
+map('n', ';', '<C-w>', {})				-- move between windows
+map('t', 'jk', [[<C-\><C-n>]], {})		-- exit terminal mode
+map('n', 'f', ':FloatermToggle<CR>', {})-- open floaterm
+map('n', 'n', ':Files<CR>', {})			-- open fzf Files ./
+map('n', 'N', ':Files ', {})			-- open fzf Files [user input]
+map('n', 'z', ':Buffers<CR>', {})
+map('n', 'Z', ':GFiles?<CR>', {})
+
+
+vim.keymap.set("n", "<localleader>p", require("nabla").popup, {})
+vim.keymap.set("n", "<localleader>s", require("nabla").toggle_virt, {})
+
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = '*',
+	callback = function ()
+		if vim.bo.filetype == 'fzf' then
+			vim.keymap.set('n', 'n', function ()
+				vim.api.nvim_command(':q')
+				vim.api.nvim_command(":lua vim.keymap.set('n', 'n', ':Files<CR>', {})")
+			end, {})
+
+			vim.keymap.set('n', 'z', function ()
+				vim.api.nvim_command(':q')
+				vim.api.nvim_command(":lua vim.keymap.set('n', 'z', ':Buffers<CR>', {})")
+			end, {})
+
+			vim.keymap.set('n', 'Z', function ()
+				vim.api.nvim_command(':q')
+				vim.api.nvim_command(":lua vim.keymap.set('n', 'Z', ':GFiles?<CR>', {})")
+			end, {})
+		end
+	end,
+})
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
