52aa33466faea68fc72ce1b2592ab4ecf1ddcdf2
Wiener234 <wiemernils@gmail.com>
2023-09-05 18:33:15 +0200

    neovim keys to toggle fzf and rewrite gruvbox

--- nvim/lua/gruvbox_conf.lua
+++ nvim/lua/gruvbox_conf.lua
@@ -1,28 +1,22 @@
--- [[ Gruvbox.lua ]]
-
--- setup must be called before loading the colorscheme
--- Default options:
 require("gruvbox").setup({
-  undercurl = true,
-  underline = true,
-  bold = true,
-  italic = {
-    strings = true,
-    comments = true,
-    operators = true,
-    folds = true,
-  },
-  strikethrough = true,
-  invert_selection = false,
-  invert_signs = false,
-  invert_tabline = false,
-  invert_intend_guides = false,
-  inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "", -- can be "hard", "soft" or empty string
-  palette_overrides = {},
-  overrides = {},
-  dim_inactive = false,
-  transparent_mode = false,
+undercurl = true,
+underline = true,
+bold = true,
+italic = {
+  strings = true,
+  comments = true,
+  operators = true,
+  folds = true,
+},
+strikethrough = true,
+invert_selection = false,
+invert_signs = false,
+invert_tabline = false,
+invert_intend_guides = false,
+inverse = true, -- invert background for search, diffs, statuslines and errors
+contrast = "", -- can be "hard", "soft" or empty string
+palette_overrides = {},
+overrides = {},
+dim_inactive = false,
+transparent_mode = false,
 })
-
-vim.g.gruvbox_italicize_strings = true
--- nvim/lua/keys.lua
+++ nvim/lua/keys.lua
@@ -10,10 +10,35 @@ map('t', 'jk', [[<C-\><C-n>]], {})		-- exit terminal mode
 map('n', 'f', ':FloatermToggle<CR>', {})-- open floaterm
 map('n', 'n', ':Files<CR>', {})			-- open fzf Files ./
 map('n', 'N', ':Files ', {})			-- open fzf Files [user input]
-map('n', ']', ':Buffers<CR>', {})
--- nnoremap <leader>p :lua require("nabla").popup()<CR> -- Customize with popup({border = ...})  : `single` (default), `double`, `rounded`
-vim.keymap.set("n", "<localleader>p", require("nabla").popup)
-vim.keymap.set("n", "<localleader>s", require("nabla").toggle_virt)
+map('n', 'z', ':Buffers<CR>', {})
+map('n', 'Z', ':GFiles?<CR>', {})
+
+
+-- map("n", "<localleader>p", require("nabla").popup, {})
+-- map("n", "<localleader>s", require("nabla").toggle_virt, {})
+
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = '*',
+	callback = function ()
+		if vim.bo.filetype == 'fzf' then
+			vim.keymap.set('n', 'n', function ()
+				vim.api.nvim_command(':q')
+				vim.api.nvim_command(":lua vim.keymap.set('n', 'n', ':Files<CR>', {})")
+			end, {})
+
+			vim.keymap.set('n', 'z', function ()
+				vim.api.nvim_command(':q')
+				vim.api.nvim_command(":lua vim.keymap.set('n', 'z', ':Buffers<CR>', {})")
+			end, {})
+
+			vim.keymap.set('n', 'Z', function ()
+				vim.api.nvim_command(':q')
+				vim.api.nvim_command(":lua vim.keymap.set('n', 'Z', ':GFiles?<CR>', {})")
+			end, {})
+		end
+	end,
+})
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
