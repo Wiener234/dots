1e0bdecdc1e6a6c3120c9e05dad223d2e8331f62
Wiener234 <wiemernils@gmail.com>
2023-09-07 22:18:09 +0200

    not a real difference

--- i3/nm-applet.sh
+++ i3/nm-applet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp /home/nils/Bilder/network-wired-activated.svg /usr/share/icons/breeze/status/22/ 
+sudo cp /home/nils/Bilder/network-wired-activated.svg /usr/share/icons/breeze/status/22/ 
 killall nm-applet
 nm-applet
